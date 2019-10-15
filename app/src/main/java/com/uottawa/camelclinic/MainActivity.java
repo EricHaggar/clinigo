5ef77e83233c669ce0e707e1630919ef4580639d
Adel Araji <aaraj081@uottawa.ca>
2019-10-15 15:21:55 -0400

    Added the user selection screen and linked it to the 'create new account' button

--- app/src/main/java/com/uottawa/camelclinic/MainActivity.java
+++ app/src/main/java/com/uottawa/camelclinic/MainActivity.java
@@ -1,6 +1,8 @@
 package com.uottawa.camelclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToUserTypeSelectionScreen (View view) {
+        Intent intent = new Intent(this, UserTypeSelection.class);
+
+        startActivity(intent);
     }
 }
