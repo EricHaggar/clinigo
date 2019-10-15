782deaa9ca28e45d436abc646c8064c5cb163d4c
Eric Haggar <ehagg014@uottawa.ca>
2019-10-15 19:01:57 -0400

    Init Firebase setup

--- app/src/main/java/com/uottawa/camelclinic/CreatingNewAccount.java
+++ app/src/main/java/com/uottawa/camelclinic/CreatingNewAccount.java
@@ -1,10 +1,21 @@
 package com.uottawa.camelclinic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreatingNewAccount extends AppCompatActivity {
 
@@ -15,6 +26,15 @@ public class CreatingNewAccount extends AppCompatActivity {
     }
 
     public void goToWelcomeNewUser (View view) {
+        EditText firstNameEditText;
+        EditText lastNameEditText;
+        EditText passwordEditText;
+        EditText confirmPassEditText;
+        EditText role;
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+
         Intent intent = new Intent(this, NewAccountWelcome.class);
 
         startActivity(intent);
