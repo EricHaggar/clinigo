cd69895acfdc34a23122c1a0cf43f5805010ec64
Eric Haggar <ehagg014@uottawa.ca>
2019-10-18 02:06:28 -0400

    Created utilities package for helper classes

---
+++ app/src/main/java/com/uottawa/camelclinic/utilities/EditTextUtilities.java
@@ -0,0 +1,21 @@
+package com.uottawa.camelclinic.utilities;
+
+import android.text.TextUtils;
+import android.widget.EditText;
+
+public class EditTextUtilities {
+
+    public static boolean allInputsFilled(String[] inputs, EditText[] fields) {
+
+        boolean inputsFilled = true;
+
+        for (int i = 0; i < inputs.length; i++) {
+            if (TextUtils.isEmpty(inputs[i])) {
+                fields[i].setError("Field cannot be empty!");
+                inputsFilled = false;
+            }
+        }
+        return inputsFilled;
+    }
+
+}
---
+++ app/src/main/java/com/uottawa/camelclinic/utilities/EmailUtilities.java
@@ -0,0 +1,7 @@
+package com.uottawa.camelclinic.utilities;
+
+public class EmailUtilities {
+    public static String createEmail(String username) {
+        return username + "@camelclinic.ca";
+    }
+}
