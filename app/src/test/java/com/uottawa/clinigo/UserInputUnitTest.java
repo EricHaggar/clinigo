b3766c4a7ea3a63af4060717cb1724d624f326a8
Eric Haggar <ehagg014@uottawa.ca>
2019-11-07 18:16:39 -0500

    Adding some unit tests

--- app/src/test/java/com/uottawa/clinigo/UserInputUnitTest.java
+++ app/src/test/java/com/uottawa/clinigo/UserInputUnitTest.java
@@ -1,21 +1,33 @@
 package com.uottawa.clinigo;
 import org.junit.Test;
 import com.uottawa.clinigo.utilities.ValidationUtilities;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class UserInputUnitTest {
 
     @Test
     public void checkValidFirstName(){
-        boolean valid = ValidationUtilities.isValidName("Dude");
-        assertEquals("check if name is valid", true, valid);
+        boolean valid = ValidationUtilities.isValidName("Marc");
+        assertEquals("Check if first name is valid", true, valid);
     }
 
     @Test
     public void checkInvalidFirstName(){
-        boolean valid = ValidationUtilities.isValidName("test@");
-        assertEquals("check if name is not valid",false, valid);
+        boolean valid = ValidationUtilities.isValidName("Eric1");
+        assertEquals("Check if first name is invalid",false, valid);
+    }
+
+    @Test
+    public void checkValidLastName() {
+        boolean valid = ValidationUtilities.isValidName("Bastawros-Araji");
+        assertEquals("Check if last name is valid",true, valid);
+    }
+
+    @Test
+    public void checkInvalidLastName() {
+        boolean valid = ValidationUtilities.isValidName("Bastawros_Araji10");
+        assertEquals("Check if last name is invalid",false, valid);
     }
 
     @Test
