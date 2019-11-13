5b96209067e21e70b724e108f40b2d4ade6c7d3a
Adel Araji <adel_araji@hotmail.com>
2019-11-13 15:42:49 -0500

    New implementation to only display the services that have been previously been added by the clinc

--- app/src/main/java/com/uottawa/clinigo/activities/EmployeeServicesActivity.java
+++ app/src/main/java/com/uottawa/clinigo/activities/EmployeeServicesActivity.java
@@ -5,11 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.uottawa.clinigo.R;
+import com.uottawa.clinigo.adapters.ServiceAdapter;
+import com.uottawa.clinigo.model.Service;
+
+import java.util.ArrayList;
 
 public class EmployeeServicesActivity extends AppCompatActivity {
 
+    ListView servicesListView;
+    ArrayList<Service> services;
+    DatabaseReference servicesReference;
     String userId;
 
     @Override
@@ -19,8 +34,56 @@ public class EmployeeServicesActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         userId = intent.getStringExtra("userId");
-        System.out.println("AdelHI");
-        System.out.println(userId);
+
+        servicesReference = FirebaseDatabase.getInstance().getReference("services");
+        servicesListView = findViewById(R.id.list_services);
+        services = new ArrayList<>();
+
+        //TODO: Below is to be overwritten when implementing the option for the user to delete the service
+
+//        servicesListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Service service = services.get(i);
+//                if (service.getEmployees() == null) {
+//                    service.initializeEmptyEmployeeArray();
+//                }
+//                showUpdateServiceDialog(service);//Adding employee Id to the service
+//                return true;
+//            }
+//        });
+
+        TextView emptyText = findViewById(R.id.text_empty_services);
+        servicesListView.setEmptyView(emptyText);
+
+        userId = getIntent().getStringExtra("userId");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        servicesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                services.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Service service = postSnapshot.getValue(Service.class);
+                    if (service.getEmployees() != null) {
+                        if (clinicHasThisService(service.getEmployees())) // check if user Id is in the arraylist
+                            services.add(service);
+                    }
+                }
+
+                ServiceAdapter serviceAdapter = new ServiceAdapter(EmployeeServicesActivity.this, services);
+                servicesListView.setAdapter(serviceAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
@@ -31,5 +94,16 @@ public class EmployeeServicesActivity extends AppCompatActivity {
         intent.putExtra("userId",userId);
 
         startActivity(intent);
+    }
+
+    public boolean clinicHasThisService (ArrayList<String> employees) {
+        boolean returnValue = false;
+
+        for (int i =0; i < employees.size(); i++) {
+            if (employees.get(i).equals(userId))
+                returnValue = true;
+        }
+
+        return returnValue;
     }
 }
