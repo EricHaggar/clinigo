4ecff0a8e881429f6b40cdfaaaac2cce35e623a9
Eric Haggar <ehagg014@uottawa.ca>
2019-11-28 18:52:48 -0500

    Fixed bugs in functionality testing

---
+++ app/src/test/java/com/uottawa/clinigo/BookingFunctionalityTesting.java
@@ -0,0 +1,63 @@
+package com.uottawa.clinigo;
+
+import com.uottawa.clinigo.model.Booking;
+import com.uottawa.clinigo.model.ClinicBookings;
+import com.uottawa.clinigo.model.Employee;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class BookingFunctionalityTesting {
+
+    @Test
+    public void verifyActiveBooking() {
+        Booking booking = new Booking("10-01-2010", "123");
+        String status = booking.getStatus();
+        assertEquals("check active status", "Active", status);
+    }
+
+    @Test
+    public void verifyAddingFunctionality() {
+
+        Booking booking1 = new Booking("10-01-2010", "123");
+        Booking booking2 = new Booking("10-01-2010", "1234");
+        Booking booking3 = new Booking("11-01-2010", "12345");
+        Employee tester = new Employee("1234", "test@test.com", "Marc", "Bastawros");
+        ClinicBookings employeeBookings = new ClinicBookings();
+        tester.setClinicBookings(employeeBookings);
+
+        tester.getClinicBookings().addBooking(booking1);
+        assertEquals(15, tester.getClinicBookings().getWaitingTime(booking1.getDate()));
+
+        tester.getClinicBookings().addBooking(booking2);
+        assertEquals(30, tester.getClinicBookings().getWaitingTime(booking2.getDate()));
+
+        tester.getClinicBookings().addBooking(booking3);
+        assertEquals(15, tester.getClinicBookings().getWaitingTime(booking3.getDate()));
+
+        ArrayList<Booking> tempArr = tester.getClinicBookings().getBookingByPatientId(booking1.getPatientId());
+        assertEquals(1, tempArr.size());
+    }
+
+    @Test
+    public void verifyDeletingFunctionality() {
+
+        Booking booking1 = new Booking("10-01-2010", "123");
+        Booking booking2 = new Booking("10-01-2010", "1234");
+        Booking booking3 = new Booking("11-01-2010", "12345");
+        Employee tester = new Employee("1234", "test@test.com", "Marc", "Bastawros");
+        ClinicBookings employeeBookings = new ClinicBookings();
+        tester.setClinicBookings(employeeBookings);
+
+        tester.getClinicBookings().addBooking(booking1);
+        tester.getClinicBookings().addBooking(booking2);
+        tester.getClinicBookings().addBooking(booking3);
+
+        tester.getClinicBookings().deleteBookingForPatient(booking1.getPatientId());
+        assertEquals(15, tester.getClinicBookings().getWaitingTime(booking1.getDate()));
+    }
+}
