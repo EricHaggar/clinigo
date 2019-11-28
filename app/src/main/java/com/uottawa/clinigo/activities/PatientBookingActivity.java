c3008d5131922a7ca50bd1b72160e1304e56c17b
Mark <mbast.amin97@gmail.com>
2019-11-27 20:59:13 -0500

    Added patient delete booking with dialog

--- app/src/main/java/com/uottawa/clinigo/activities/PatientBookingActivity.java
+++ app/src/main/java/com/uottawa/clinigo/activities/PatientBookingActivity.java
@@ -3,8 +3,14 @@ package com.uottawa.clinigo.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ThrowOnExtraProperties;
 import com.google.firebase.database.ValueEventListener;
 import com.uottawa.clinigo.R;
 import com.uottawa.clinigo.adapters.PatientBookingsAdapter;
@@ -27,6 +34,7 @@ public class PatientBookingActivity extends AppCompatActivity {
     private DatabaseReference patientReference;
     private ArrayList<Booking> patientArrayOfBookings;
     private TextView patientHasNoBookings;
+    private String[] patientBookingDelete;
     ListView bookingsListView;
 
 
@@ -71,5 +79,45 @@ public class PatientBookingActivity extends AppCompatActivity {
         final PatientBookingsAdapter patientBookingsAdapter = new PatientBookingsAdapter(PatientBookingActivity.this, results);
         bookingsListView.setAdapter(patientBookingsAdapter);
         bookingsListView.setClickable(true);
+        bookingsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                patientBookingDelete[0] = patientBookingsAdapter.getItem(position).getClinicName();
+                patientBookingDelete[1] = patientBookingsAdapter.getItem(position).getDate();
+            }
+        });
+    }
+
+    public void delete_booking(View view){
+
+        final int position = bookingsListView.getPositionForView(view);
+        android.app.AlertDialog.Builder dialogBuilder = new android.app.AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.delete_booking_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final Button deleteButton = dialogView.findViewById(R.id.button_delete_booking);
+        final Button cancelButton = dialogView.findViewById(R.id.button_cancel_noDelete);
+
+        dialogBuilder.setTitle("Delete Booking");
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b.dismiss();
+                Toast.makeText(getApplicationContext(), "removed : "+ position, Toast.LENGTH_LONG).show();
+                patientArrayOfBookings.remove(position);
+                displayResults(patientArrayOfBookings);
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b.dismiss();
+            }
+        });
+
     }
 }
